5a4a1bf492659527272429a3009787aad2024bb6
Hoan Nguyen <nguyenvanhoan02@gmail.com>
2020-12-18 15:21:37 +0700

    Finalize square box

--- src/components/square-box/index.js
+++ src/components/square-box/index.js
@@ -1,6 +1,29 @@
-import React from 'react';
+import React, { forwardRef, useState, useEffect, useCallback } from 'react';
 import './index.scss';
 
-const SquareBox = () => <span className="square-box" />;
+const SquareBox = forwardRef(({ transform, speed }, ref) => {
+    // Get transition based on speed number
+    const getTransition = useCallback((speed) => {
+        if (!speed) return 'transform 0s cubic-bezier(0.075, 0.82, 0.165, 1)';
+        return `transform ${speed}s cubic-bezier(0.075, 0.82, 0.165, 1)`;
+    }, []);
+
+    const [transition, setTransition] = useState(getTransition(speed));
+
+    useEffect(() => {
+        setTransition(getTransition(speed));
+    }, [getTransition, speed]);
+
+    return (
+        <span
+            ref={ref}
+            style={{
+                transition,
+                ...(transform && { transform }),
+            }}
+            className="square-box"
+        />
+    );
+});
 
 export default SquareBox;
