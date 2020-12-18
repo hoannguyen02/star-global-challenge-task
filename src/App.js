ebff27a4b900f07b1ca90a43ca6ef7b088d47854
Hoan Nguyen <nguyenvanhoan02@gmail.com>
2020-12-18 08:42:30 +0700

    Structure App: Add input component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import SquareBox from './Components/SquareBox';
+import Input from './Components/Input';
 
 const App = () => {
     return (
         <div className="App">
             <SquareBox />
+            <Input />
         </div>
     );
 };
---
+++ src/Components/Input.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Input = () => (
+    <div className="input">
+        <label>Input works!</label>
+        <input />
+    </div>
+);
+
+export default Input;
